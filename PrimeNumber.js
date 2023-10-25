ede3ca35ac13817628dd7b69c802284a296c9a51
unknown <sanketpatil1079@gmail.com>
2023-10-25 18:25:47 +0530

    code fixed

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,6 +1,6 @@
 function isPrime(num) { 
     if (num <= 1) return false; 
-    if (num % 2 == 0 || num > 2) return false; 
+    if (num % 2 == 0 && num > 2) return false; 
     for(let i = 3; i <= s; i += 2) { 
         if(num % i === 0) return false; 
     }
